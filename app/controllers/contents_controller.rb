fea9ba1c51fcd3ecb31ffc760157762d0cc4ec30
Ryosuke Uchida <uchiryo7@gmail.com>
2017-04-23 14:35:17 +0900

    Edited form file

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -69,6 +69,6 @@ class ContentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def content_params
-      params.fetch(:content, {})
+      params.require(:content).permit(:title, :sentence, :picture)
     end
 end
